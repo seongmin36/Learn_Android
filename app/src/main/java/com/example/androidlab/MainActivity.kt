adc2b30297d572e7d995670ae510e3a1ad7f4cb0
seongmin36 <whtjdals3625@gmail.com>
2025-10-01 23:45:42 +0900

    feat: 타이머 프로젝트 구현

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.example.androidlab
 
 import android.os.Bundle
-import android.os.SystemClock
+import android.os.CountDownTimer
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var pauseTime = 0L
-
-   lateinit  var binding : ActivityMainBinding
+    lateinit var binding : ActivityMainBinding
+    var timer: CountDownTimer? = null
+    var timeLeft: Long = 0L
+    var isPaused = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        setContentView(binding.root)
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -25,27 +23,49 @@ class MainActivity : AppCompatActivity() {
 
         // 시작 버튼
         binding.startButton.setOnClickListener {
-            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
-            binding.chronometer.start()
+            val input = binding.inputTime.text.toString()
+            if(input.isEmpty()) return@setOnClickListener
+
+            if(!isPaused) {
+                timeLeft = input.toLong() * 1000
+            }
+
+            // CountDown을 실행시켜 주는 android.os 내장 메서드
+            timer = object : CountDownTimer(timeLeft, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    timeLeft = millisUntilFinished
+                    val totalSeconds = millisUntilFinished / 1000
+                    val minutes = (totalSeconds / 60).toString().padStart(2, '0')
+                    val seconds = (totalSeconds % 60).toString().padStart(2, '0')
+                    binding.timerText.text = "$minutes:$seconds"
+                }
+
+                // 타이머 종료 이벤트
+                override fun onFinish() {
+                    binding.timerText.text="00:00"
+                    Toast.makeText(this@MainActivity, "타이머 종료!", Toast.LENGTH_SHORT).show()
+                    updateButtonState(start = true, stop=false, reset=false)
+                    isPaused=false
+                }
+            }.start()   // 결론적으로 timer.start()가 된다.
 
             updateButtonState(start = false, stop = true, reset = true)
+            isPaused=false
         }
 
         // 일시 정지 버튼
         binding.stopButton.setOnClickListener {
-            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
-            binding.chronometer.stop()
-
+            timer?.cancel()
+            isPaused = true
             updateButtonState(start = true, stop = false, reset = true)
         }
 
         // 초기화 버튼
         binding.resetButton.setOnClickListener {
-            pauseTime = 0L
-            binding.chronometer.base = SystemClock.elapsedRealtime()
-            binding.chronometer.stop()
-
-            Toast.makeText(this, "타이머 초기화!", Toast.LENGTH_SHORT).show()
+            timer?.cancel()
+            binding.timerText.text="00:00"
+            timeLeft=0L
+            isPaused=false
             updateButtonState(start = true, stop = false, reset = false)
         }
     }

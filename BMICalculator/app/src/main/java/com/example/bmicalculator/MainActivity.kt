932d3cb8a32fc638a5899035a161f2e7e50e7396
seongmin36 <whtjdals3625@gmail.com>
2025-10-27 17:52:01 +0900

    feat: activity분리 및 intent 설정 구현

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
@@ -7,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bmicalculator.databinding.ActivityMainBinding
+import com.example.bmicalculator.utils.BMIUtils
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,7 +38,13 @@ class MainActivity : AppCompatActivity() {
         }
         // BMI 계산 결과 출력
         binding.bmiCalc.setOnClickListener {
-            resultBMI()
+            val bmi = BMIUtils.calculateBMI(binding.inputKg, binding.inputCm)
+
+            // 인텐트 생성
+            val intent = Intent(this, ResultBMI::class.java)
+            // 데이터 전달 (putExtra)
+            intent.putExtra("bmiResult", bmi)
+            startActivity(intent)
         }
     }
 
@@ -51,50 +59,5 @@ class MainActivity : AppCompatActivity() {
         val current = editText.text.toString().toIntOrNull() ?: 0
         val newValue = if (current > 0) current - 1 else 0
         editText.setText(newValue.toString())
-    }
-
-    // BMI 측정값
-    private fun calculateBMI(weight: EditText, height: EditText): Double {
-        val kg = weight.text.toString().toDoubleOrNull() ?: 0.00
-        val cm = height.text.toString().toDoubleOrNull() ?: 0.00
-
-        val m = cm / 100
-
-        val bmi: Double = kg / (m * m)
-
-        return bmi
-    }
-
-    // BMI 측정 결과
-    private fun resultBMI() {
-        val bmi = calculateBMI(binding.inputKg, binding.inputCm)
-        binding.bmiNum.text = String.format("당신의 BMI는 %.2f", bmi)
-
-        when {
-            bmi == 0.00 -> {
-                binding.bmiStatus.text = "입력 오류"
-                binding.bmiEx.text = "키와 몸무게를 올바르게 입력해주세요."
-            }
-
-            bmi < 18.50 -> {
-                binding.bmiStatus.text = "저체중"
-                binding.bmiEx.text = "식사량을 늘리세요."
-            }
-
-            bmi in 18.5..24.99 -> {
-                binding.bmiStatus.text = "정상"
-                binding.bmiEx.text = "축하합니다. 현재 상태를 유지하세요."
-            }
-
-            bmi in 25.50..29.99 -> {
-                binding.bmiStatus.text = "과체중"
-                binding.bmiEx.text = "운동과 식사량을 조절하세요."
-            }
-
-            bmi >= 30.00 -> {
-                binding.bmiStatus.text = "비만"
-                binding.bmiEx.text = "위험한 단계입니다. 다이어트와 운동이 필요합니다."
-            }
-        }
     }
 }

50cf638013e764a0c34d37ad2b96218de8be2e3b
seongmin36 <whtjdals3625@gmail.com>
2025-10-30 03:19:39 +0900

    feat: 채팅앱 구현 (채팅 기록 저장 X)

--- PracMidterm/app/src/main/java/com/example/pracmidterm/VoteActivity.kt
+++ PracMidterm/app/src/main/java/com/example/pracmidterm/VoteActivity.kt
@@ -2,7 +2,6 @@ package com.example.pracmidterm
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pracmidterm.databinding.ActivityVoteBinding
